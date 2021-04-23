9f3a679729c2053c3d5a0dba5bb19d1770096c7d
unknown <haffedmcnair@gmail.com>
2021-04-23 13:36:25 -0700

    added the questions above their respective functions as comments

--- PythonExercises.py
+++ PythonExercises.py
@@ -11,6 +11,15 @@ def monthGoesBy(lumpSum,interestRate,minimumPayment):
 monthGoesBy(lumpSum, interestRate, minimumPayment)
 
 
+
+
+# Write a program which will find all 
+# such numbers which are divisible by 7 
+# but are not a multiple of 5,between 2000 
+#  3200 (both included).The numbers obtained 
+#  should be printed in a comma-separated 
+#  sequence on a single line
+
 def divisbleBySevenNotByFive():
     results = []
     x = range(2000,3000)
@@ -19,6 +28,9 @@ def divisbleBySevenNotByFive():
             results.append(n)
     print(results)
 
+# Write a program which can compute the factorial of a 
+# given numbers.The results should be printed in a 
+# comma-separated sequence on a single line
 
 def factorialRecursive(n):
     if n < 2:
@@ -54,11 +66,36 @@ class StringClass():
         print(self.string)
 
 
+# Your goal in this programming exercise is to
+#  determine, whether the phrase represents a 
+#  palindrome or not.Input datacontains number 
+#  of phrases in the first line.Next lines contain 
+#  one phrase each.Answershould have a single 
+#  letter (space separated) for each phrase:Y 
+#  if it is a palindrome andNif not.
 
+def isPalindrome(string):
+    place = -1
+    flag = 0
+    for i in string:
+        if i.lower != string[place].lower:
+            place = place - 1
+            flag = 1
+            break
+        place = place - 1
+    if flag == 1:
+        return("N ")
+    else:
+        return("Y ")
 
-# stringRetriever = StringClass()
-# stringRetriever.setString()
-# stringRetriever.printString()
+def checkPhrases(listOfPhrases):
+    returnString = ""
+    for i in listOfPhrases:
+        returnString += isPalindrome(i)
+    print(returnString)
+
 
 print(factorialRecursive(8))
 allIntegrals(10)
+
+checkPhrases(["racecar", "president", "Some men interpret nine memos"])
